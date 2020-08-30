f16cd82e8964e2b64747df354eed1b8b40584e94
Aremarrahhh35 <42797460+Aremarrahhh35@users.noreply.github.com>
2020-08-30 21:15:53 +0100

    Month added
    I added month and the buttons to start over and quit

--- birthdateGuesser.py
+++ birthdateGuesser.py
@@ -5,7 +5,8 @@ HEIGHT = 450
 WIDTH = 550
 
 date = 0
-
+month = 0
+finaldate =""
 
 root = tk.Tk()
 root.resizable(False, False)
@@ -16,18 +17,20 @@ background_image = tk.PhotoImage(file='birthdate.png')
 setbackground_image = tk.PhotoImage(file='birthdate1.png')
 firebackground_image = tk.PhotoImage(file='firebirth2.png')
 
+
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg='green')
 canvas.pack()
 
-coverbg = tk.Frame(root, bg='pink')
-coverbg.place(relwidth=1, relheight=1)
+def openingPage():
+    coverbg = tk.Frame(root, bg='pink')
+    coverbg.place(relwidth=1, relheight=1)
 
-coverpage = tk.Frame(coverbg, bg='pink')
-coverpage.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
+    coverpage = tk.Frame(coverbg, bg='pink')
+    coverpage.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
 
-opentext = tk.Button(coverpage,text='I CAN GUESS YOUR BIRTH DATE!\n Click anywhere to continue', image=background_image, font='Helvetica 17 bold',bg='pink', relief='flat', command=lambda :setA())
-opentext.place(relwidth=1, relheight=1)
-
+    opentext = tk.Button(coverpage,text='I CAN GUESS YOUR BIRTH DATE!\n Click anywhere to continue', image=background_image, font='Helvetica 17 bold',bg='pink', relief='flat', command=lambda :setA())
+    opentext.place(relwidth=1, relheight=1)
+openingPage()
 
 def setA():
     setbackground = tk.Frame(root, bg='gray')
@@ -196,75 +199,269 @@ def noE():
 
 def birthdate():
     global date
+    global finaldate
+    datewithmonth = date
     if date == 0:
-        finaldate = "You don't have a Birth Date!"
+        finaldate = ""
     elif date == 1:
-        finaldate = "You were born on the 1ST!"
+        finaldate = " 1ST"
     elif date == 2:
-        finaldate = "You were born on the 2ND!"
+        finaldate = " 2ND"
     elif date == 3:
-        finaldate = "You were born on the 3RD!"
+        finaldate = " 3RD"
     elif date == 4:
-        finaldate = "You were born on the 4TH!"
+        finaldate = " 4TH"
     elif date == 5:
-        finaldate = "You were born on the 5TH!"
+        finaldate = " 5TH"
     elif date == 6:
-        finaldate = "You were born on the 6TH!"
+        finaldate = " 6TH"
     elif date == 7:
-        finaldate = "You were born on the 7TH!"
+        finaldate = " 7TH"
     elif date == 8:
-        finaldate="You were born on the 8TH!"
+        finaldate=" 8TH"
     elif date == 9:
-        finaldate="You were born on the 9TH!"
+        finaldate=" 9TH"
     elif date == 10:
-        finaldate="You were born on the 10TH!"
+        finaldate=" 10TH"
     elif date == 11:
-        finaldate="You were born on the 11TH!"
+        finaldate=" 11TH"
     elif date == 12:
-        finaldate="You were born on the 12TH!"
+        finaldate=" 12TH"
     elif date == 13:
-        finaldate="You were born on the 13TH!"
+        finaldate=" 13TH"
     elif date == 14:
-        finaldate="You were born on the 14TH!"
+        finaldate=" 14TH"
     elif date == 15:
-        finaldate="You were born on the 15TH!"
+        finaldate=" 15TH"
     elif date == 16:
-        finaldate="You were born on the 16TH!"
+        finaldate=" 16TH"
     elif date == 17:
-        finaldate="You were born on the 17TH!"
+        finaldate=" 17TH"
     elif date == 18:
-        finaldate="You were born on the 18TH!"
+        finaldate=" 18TH"
     elif date == 19:
-        finaldate="You were born on the 19TH!"
+        finaldate=" 19TH"
     elif date == 20:
-        finaldate="You were born on the 20TH!"
+        finaldate=" 20TH"
     elif date == 21:
-        finaldate="You were born on the 21ST!"
+        finaldate=" 21ST"
     elif date == 22:
-        finaldate="You were born on the 22ND!"
+        finaldate=" 22ND"
     elif date == 23:
-        finaldate="You were born on the 23RD!"
+        finaldate=" 23RD"
     elif date == 24:
-        finaldate="You were born on the 24TH!"
+        finaldate=" 24TH"
     elif date == 25:
-        finaldate="You were born on the 25TH!"
+        finaldate=" 25TH"
     elif date == 26:
-        finaldate="You were born on the 26TH!"
+        finaldate=" 26TH"
     elif date == 27:
-        finaldate="You were born on the 27TH!"
+        finaldate=" 27TH"
     elif date == 28:
-        finaldate="You were born on the 28TH!"
+        finaldate=" 28TH"
     elif date == 29:
-        finaldate="You were born on the 29TH!"
+        finaldate=" 29TH"
     elif date == 30:
-        finaldate="You were born on the 30TH!"
+        finaldate=" 30TH"
     else:
-        finaldate="You were born on the 31ST!"
+        finaldate=" 31ST"
+    anotherfinaldate = finaldate
+
+
+    date = 0
+    monthA()
+    return finaldate
+
+
+
+def monthA():
+    setbackground = tk.Frame(root, bg='gray')
+    setbackground.place(relwidth=1, relheight=1)
+    setbackgroundlabel = tk.Label(setbackground, image=setbackground_image)
+    setbackgroundlabel.place(relwidth=1, relheight=1)
+
+    monthAFrame = tk.Frame(setbackground, bg='#9C5AFD')
+    monthAFrame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
+
+    monthAQuestion = tk.Label(monthAFrame, text='Is Your Birth Date In This Set Of Dates?', font='Helvetica 14 bold', bg='#9C5AFD')
+    monthAQuestion.place(relx=0.5, rely=0.05, relwidth=0.65, relheight=0.1, anchor='n')
+
+    monthADates = tk.Label(monthAFrame,
+                         text='\nJAN\tMAR\tMAY\n\n\nJUL\tSEP\tNOV\n',
+                         font='Helvetica 17 bold')
+
+    monthADates.place(relx=0.5, rely=0.20, relwidth=0.8, relheight=0.6, anchor='n')
+
+    AYes = tk.Button(monthAFrame, text='YES', font='Helvetica 12 bold', command=lambda:monthYesA())
+    AYes.place(relx=0.1, rely=0.85, relwidth=0.2, relheight=0.1)
+
+    ANo = tk.Button(monthAFrame, text='NO', font='Helvetica 12 bold', command=lambda:monthNoA())
+    ANo.place(relx=0.7, rely=0.85, relwidth=0.2, relheight=0.1)
+
+def monthYesA():
+    global month
+    month = month + 1
+    monthB()
+    return month
+
+def monthNoA():
+    global month
+    month = month + 0
+    monthB()
+    return month
+
+
+def monthB():
+    monthBFrame = tk.Frame(root, bg='#FDB0FE')
+    monthBFrame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
+
+    monthBQuestion = tk.Label(monthBFrame, text='Is Your Birth Date In This Set Of Dates?', font='Helvetica 14 bold',
+                              bg='#FDB0FE')
+    monthBQuestion.place(relx=0.5, rely=0.05, relwidth=0.65, relheight=0.1, anchor='n')
+
+    monthADates = tk.Label(monthBFrame,
+                           text='\nFEB\tMAR\tJUN\n\n\nJUL\tOCT\tNOV\n',
+                           font='Helvetica 17 bold')
+
+    monthADates.place(relx=0.5, rely=0.20, relwidth=0.8, relheight=0.6, anchor='n')
+
+    BYes = tk.Button(monthBFrame, text='YES', font='Helvetica 12 bold', command=lambda: monthYesB())
+    BYes.place(relx=0.1, rely=0.85, relwidth=0.2, relheight=0.1)
+
+    ANo = tk.Button(monthBFrame, text='NO', font='Helvetica 12 bold', command=lambda: monthNoB())
+    ANo.place(relx=0.7, rely=0.85, relwidth=0.2, relheight=0.1)
+
+
+def monthYesB():
+    global month
+    month = month + 2
+    monthC()
+    return month
+
+
+def monthNoB():
+    global month
+    month = month + 0
+    monthC()
+    return month
+
+
+def monthC():
+    monthCFrame = tk.Frame(root, bg='#27DDFE')
+    monthCFrame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
+
+    monthCQuestion = tk.Label(monthCFrame, text='Is Your Birth Date In This Set Of Dates?', font='Helvetica 14 bold',
+                              bg='#27DDFE')
+    monthCQuestion.place(relx=0.5, rely=0.05, relwidth=0.65, relheight=0.1, anchor='n')
+
+    monthCDates = tk.Label(monthCFrame,
+                           text='\nAPR\tMAY\tJUN\n\n\nJUL\tDEC\tXXX\n',
+                           font='Helvetica 17 bold')
+
+    monthCDates.place(relx=0.5, rely=0.20, relwidth=0.8, relheight=0.6, anchor='n')
+
+    CYes = tk.Button(monthCFrame, text='YES', font='Helvetica 12 bold', command=lambda: monthYesC())
+    CYes.place(relx=0.1, rely=0.85, relwidth=0.2, relheight=0.1)
+
+    CNo = tk.Button(monthCFrame, text='NO', font='Helvetica 12 bold', command=lambda: monthNoC())
+    CNo.place(relx=0.7, rely=0.85, relwidth=0.2, relheight=0.1)
+
+
+def monthYesC():
+    global month
+    month = month + 4
+    monthD()
+    return month
+
+
+def monthNoC():
+    global month
+    month = month + 0
+    monthD()
+    return month
+
+
+def monthD():
+    monthDFrame = tk.Frame(root, bg='#02AD99')
+    monthDFrame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
+
+    monthDQuestion = tk.Label(monthDFrame, text='Is Your Birth Date In This Set Of Dates?', font='Helvetica 14 bold',
+                              bg='#02AD99')
+    monthDQuestion.place(relx=0.5, rely=0.05, relwidth=0.65, relheight=0.1, anchor='n')
+
+    monthDDates = tk.Label(monthDFrame,
+                           text='\nAUG\tSEP\tOCT\n\n\nNOV\tDEC\tXXX\n',
+                           font='Helvetica 17 bold')
+
+    monthDDates.place(relx=0.5, rely=0.20, relwidth=0.8, relheight=0.6, anchor='n')
+
+    CYes = tk.Button(monthDFrame, text='YES', font='Helvetica 12 bold', command=lambda: monthYesD())
+    CYes.place(relx=0.1, rely=0.85, relwidth=0.2, relheight=0.1)
+
+    CNo = tk.Button(monthDFrame, text='NO', font='Helvetica 12 bold', command=lambda: monthNoD())
+    CNo.place(relx=0.7, rely=0.85, relwidth=0.2, relheight=0.1)
+
+
+def monthYesD():
+    global month
+    month = month + 8
+    birthday()
+    return month
+
+
+def monthNoD():
+    global month
+    month = month + 0
+    birthday()
+    return month
+
+def birthday():
+    global month
+    global finaldate
+    datewithmonth = date
+    if  finaldate == "":
+        finalmonth = "You don't have a Birthday!\n Try Again"
+    elif month == 0:
+        finalmonth = "You don't have a Birthday!\n Try Again"
+    elif finaldate == "" and month == 0:
+        finalmonth = "You don't have a Birthday!\n Try Again"
+    elif month == 1:
+        finalmonth = "You were born on the\n" + finaldate + " of JANUARY!"
+    elif month == 2:
+        finalmonth = "You were born on the\n" + finaldate + " of FEBRUARY!"
+    elif month == 3:
+        finalmonth = "You were born on the\n" + finaldate + " of MARCH!"
+    elif month == 4:
+        finalmonth = "You were born on the\n" + finaldate + " of APRIL!"
+    elif month == 5:
+        finalmonth = "You were born on the\n" + finaldate + " of MAY!"
+    elif month == 6:
+        finalmonth = "You were born on the\n" + finaldate + " of JUNE!"
+    elif month == 7:
+        finalmonth = "You were born on the\n" + finaldate + " of JULY!"
+    elif month == 8:
+        finalmonth="You were born on the\n" + finaldate + " of AUGUST!"
+    elif month == 9:
+        finalmonth="You were born on the\n" + finaldate + " of SEPTEMBER!"
+    elif month == 10:
+        finalmonth="You were born on the\n" + finaldate + " of OCTOBER!"
+    elif month == 11:
+        finalmonth="You were born on the\n" + finaldate + " of NOVEMBER!"
+    else :
+        finalmonth="You were born on the\n" + finaldate + " of DECEMBER!"
+    month = 0
+
 
     dateFrame = tk.Frame(root)
     dateFrame.place(relwidth=1, relheight=1)
-    background_label = tk.Label(dateFrame, text=finaldate, font='Helvetica 30 bold', fg='white', image=firebackground_image, compound='center')
+    background_label = tk.Label(dateFrame, text=finalmonth + '\n', font='Helvetica 28 bold', fg='white', image=firebackground_image, compound='center')
     background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    againbutton = tk.Button(dateFrame, text='Try Again!', font='Helvetica 12 bold', command= lambda:openingPage() )
+    againbutton.place(relx=0.1, rely=0.85,  relwidth=0.2, relheight=0.1)
+
+    exitbutton = tk.Button(dateFrame, text='Exit', font='Helvetica 12 bold',command = lambda :quit())
+    exitbutton.place(relx=0.7, rely=0.85, relwidth=0.2, relheight=0.1)
 
 
 root.mainloop()
